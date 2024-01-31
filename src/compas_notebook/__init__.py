aee68fbf600383ff52c1d045ee7563d1372d2d81
tomvanmele <tom.v.mele@gmail.com>
2024-01-31 12:22:01 +0100

    reset version

--- src/compas_notebook/__init__.py
+++ src/compas_notebook/__init__.py
@@ -7,7 +7,7 @@ __author__ = ["tom van mele"]
 __copyright__ = "COMPAS Association"
 __license__ = "MIT License"
 __email__ = "tom.v.mele@gmail.com"
-__version__ = "0.2.4"
+__version__ = "0.1.0"
 
 
 HERE = os.path.dirname(__file__)
