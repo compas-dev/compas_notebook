95fa78e098f08291bafc8c0da2f195e5776e4e2b
tomvanmele <tom.v.mele@gmail.com>
2024-01-31 12:22:21 +0100

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ optional_requirements = {}
 
 setup(
     name="compas_notebook",
-    version="0.1.0",
+    version="0.1.1",
     description="Notebook visualization backend for COMPAS using pythreejs",
     long_description=long_description,
     long_description_content_type="text/markdown",
